baebbbf38bcb547c0a00ca5c987311d0cefd764a
C.T. Lin <chentsulin@gmail.com>
2016-02-25 17:13:27 +0800

    0.1.1

--- test/test.js
+++ test/test.js
@@ -1,10 +1,42 @@
 import { expect } from 'chai';
-import reactWindowState from './';
+import React, { Component } from 'react';
+import {
+  createRenderer,
+  Simulate,
+} from 'react-addons-test-utils';
+import windowState from '../src';
 
 
-describe('reactWindowState', () => {
-  it('should ', () => {
-  	expect(reactWindowState('unicorns')).to.equal('unicorns & rainbows');
+class InnerComponent extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+function setup() {
+  const props = {};
+
+  const renderer = createRenderer();
+
+  const WrappedComponent = windowState(InnerComponent);
+
+  renderer.render(
+    <WrappedComponent {...props} />
+  );
+
+  const output = renderer.getRenderOutput();
+
+  return {
+    props: props,
+    output: output,
+    renderer: renderer,
+  };
+}
+
+describe('windowState', () => {
+  it('should render inner component', () => {
+    const { output } = setup();
+    expect(output.type).to.equal('div');
   });
 
   it('should ', async () => {
